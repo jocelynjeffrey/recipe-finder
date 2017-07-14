b0b6d466b803ac4442e3a8a13833854d65f70d04
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2017-07-14 11:58:05 -0400

    in progress

--- src/tests/components.test.js
+++ src/tests/components.test.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../App';
+import Details from '../components/Details';
+import ResultsList from '../components/ResultsList';
+import Search from '../components/Search';
 import { shallow } from 'enzyme';
 
 describe('component tests', function() {
-  describe('<App />', function() {
-    it('renders correctly', function() {
-      const wrapper = shallow(<App />);
-      const childComponent = <Child />;
 
-      expect(wrapper.contains(childComponent)).toEqual(true);
+  const props = {
+    list: [],
+    isError: false,
+    selectedRecipe: null,
+    inputChange: '',
+  }
+
+  describe('<Details />', function() {
+    it('renders correctly', function() {
+      const div = document.createElement('div');
+      ReactDOM.render(<Details />, div);
     });
   });
+
+  describe('<ResultsList />', function() {
+    it('renders a list', function() {
+//needs to map over props.list
+    });
+  });
+
+  describe('<Search />', function() {
+    it('renders correctly', function() {
+//needs access to input
+    });
+  });
+
+
 });
